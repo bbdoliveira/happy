6e3f6f0700224ababc39c97c26de68aa70f7103f
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-10-27 13:11:45 -0300

    add marker final

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,32 @@
+// Create Map
+const map = L.map("mapid").setView([-27.222633, -49.6455874], 15);
+
+// Create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68]
+})
+
+// Create and add marker
+let marker;
+
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    // remove icon
+    marker && map.removeLayer(marker);
+
+    // add icon layer
+    marker = L.marker([lat, lng], { icon }).addTo(map);
+})
+
+// add photo field
+
